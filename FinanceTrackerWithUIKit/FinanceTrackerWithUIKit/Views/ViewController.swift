dd9cf7beb2be08d1c8e804789149ba36a6a53015
FilovetsAlexandr <sohan.boxan@gmail.com>
2023-11-10 14:54:00 +0300

    project created

---
+++ FinanceTrackerWithUIKit/FinanceTrackerWithUIKit/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinanceTrackerWithUIKit
+//
+//  Created by Alexandr Filovets on 10.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
